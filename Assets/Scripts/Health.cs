238b9e223dece3fc0446a7ad54b63d82b04bc80d
McLovin21074 <alexanderrarapov@gmail.com>
2025-09-07 00:57:56 +0500

    HealthSAystem Added

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public interface IDamagable
+{
+    void TakeDamage(int amount);
+}
+
+public class Health : MonoBehaviour, IDamagable
+{
+    [SerializeField] private int maxHp = 5;
+    public UnityEvent onDeath;
+
+    public int Current {  get; private set; }
+
+    private void Awake()
+    {
+        Current = maxHp;
+    }
+
+    public void TakeDamage(int amount)
+    {
+        if (Current <= 0) return;
+        Current -= Mathf.Max(1, amount);
+        if(Current <= 0) onDeath?.Invoke();
+    }
+
+    public void SetMaxFromSO(int value)
+    {
+        maxHp = value;
+        Current = maxHp;
+    }
+
+}
---
+++ Assets/Scripts/PlayerSatatsSO.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "PlayerSatatsSO", menuName = "Scriptable Objects/PlayerSatatsSO")]
+public class PlayerSatatsSO : ScriptableObject
+{
+    [Header("Health")]
+    public int maxHP = 5;
+
+    [Header("Shooting")]
+    public float fireRate = 4f;
+    public float bulletSpeed = 12f;
+    public float bulletRange = 8f;
+    public int bulletDamage = 1;
+    public int bulletCount = 1;
+    public float bulletSize = 1f;
+    public float spreadDeg = 0f;
+    
+}
